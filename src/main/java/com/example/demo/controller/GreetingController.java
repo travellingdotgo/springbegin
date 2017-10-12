541ea48e0e45de14ec38b70fdddf3328286afd92
c <c@g.hk>
2017-10-12 17:08:20 +0800

    init

---
+++ src/main/java/com/example/demo/controller/GreetingController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @ApiOperation(value="删除用户", notes="根据url的id来指定删除对象")
+    @ApiImplicitParam(name = "name", value = "用户ID", required = true, dataType = "String")
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, name));
+    }
+}
---
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @RequestMapping("/index2")
+    public String index2() {
+        return "index2";
+    }
+
+    @RequestMapping("/")
+    public String hello(){
+        return "hello Springboot";
+    }
+
+}
