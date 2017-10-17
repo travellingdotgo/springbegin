5ad365db2f892c0cc62964c04204dcc3912778a1
c <c@g.hk>
2017-10-17 14:35:17 +0800

    map RestController and ConfigBean, application.yml. It works fine

---
+++ src/main/java/com/example/demo/controller/LucyController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+
+import com.example.demo.bean.ConfigBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableConfigurationProperties({ConfigBean.class})
+public class LucyController {
+    @Autowired
+    ConfigBean configBean;
+
+    @RequestMapping(value = "/lucy")
+    public String miya() {
+        return      configBean.getGreeting() + " >>>>"
+                + configBean.getName() + " >>>>"
+                + configBean.getUuid() + " >>>>"
+                + configBean.getMax();
+    }
+}
