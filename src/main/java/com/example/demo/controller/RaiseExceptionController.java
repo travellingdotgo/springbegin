9c7651dde128d98a1f3b8404468c8c25b7f5ba0d
c <c@g.hk>
2017-10-17 17:33:15 +0800

    coding style improvement

--- src/main/java/com/example/demo/controller/RaiseExceptionController.java
+++ src/main/java/com/example/demo/controller/RaiseExceptionController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class RaiseExceptionController {
     @RequestMapping("/raiseexception")
     public String json() throws MyException {
-        throw new MyException("发生错误2");
+        throw new MyException("故意抛出的异常");
     }
 }
---
+++ src/main/java/com/example/demo/exception/GlobalMyExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.example.demo.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class GlobalMyExceptionHandler {
+    @ExceptionHandler(value = MyException.class)
+    @ResponseBody
+    public ErrorInfo<String> jsonErrorHandler(HttpServletRequest req, MyException e) throws Exception {
+        ErrorInfo<String> r = new ErrorInfo<>();
+        r.setMessage(e.getMessage());
+        r.setCode(ErrorInfo.ERROR);
+        r.setData("Some Data");
+        r.setUrl(req.getRequestURL().toString());
+        return r;
+    }
+}
